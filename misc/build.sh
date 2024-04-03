235ef6d3d56d08fcd27205b736d32effec72e4be
asventetsky <artyom.sven@gmail.com>
2024-04-04 00:34:13 +0200

    Feature#29: update path to `extract_lambda_name_version.sh`

--- misc/build.sh
+++ misc/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 build_image() {
-  ARTEFACT_FOLDER=$1
-  LAMBDA_NAME_AND_VERSION=$("${ARTEFACT_FOLDER}"/../../misc/extract_lambda_name_version.sh)
+  RELATIVE_ARTIFACT_FOLDER=$1
+  LAMBDA_NAME_AND_VERSION=$("${RELATIVE_ARTIFACT_FOLDER}"/../../misc/extract_lambda_name_version.sh)
 #  LAMBDA_NAME_AND_VERSION=$(./../../../../misc/extract_lambda_name_version.sh)
 
   echo "Building the image ${LAMBDA_NAME_AND_VERSION}"
   docker build -t "${LAMBDA_NAME_AND_VERSION}" .
 
   echo "Saving the image ${LAMBDA_NAME_AND_VERSION}"
-  mkdir -p $ARTEFACT_FOLDER
-  docker save "${LAMBDA_NAME_AND_VERSION}" > "${ARTEFACT_FOLDER}"/"${LAMBDA_NAME_AND_VERSION}".tar
+  mkdir -p $RELATIVE_ARTIFACT_FOLDER
+  docker save "${LAMBDA_NAME_AND_VERSION}" > "${RELATIVE_ARTIFACT_FOLDER}"/"${LAMBDA_NAME_AND_VERSION}".tar
 }
 
 build() {
