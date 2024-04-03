5cc4b0eb8853655a70e71663833ca50ec1007b35
asventetsky <artyom.sven@gmail.com>
2024-04-04 01:12:15 +0200

    Feature#29: print folder with artifacts

--- misc/build.sh
+++ misc/build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 build_image() {
-  RELATIVE_ARTIFACT_FOLDER=$1
-  LAMBDA_NAME_AND_VERSION=$("${RELATIVE_ARTIFACT_FOLDER}"../../misc/extract_lambda_name_version.sh)
+  RELATIVE_ARTIFACT_PATH=$1
+  LAMBDA_NAME_AND_VERSION=$("${RELATIVE_ARTIFACT_PATH}"../../misc/extract_lambda_name_version.sh)
 
   echo "Building the image ${LAMBDA_NAME_AND_VERSION}"
   docker build -t "${LAMBDA_NAME_AND_VERSION}" .
 
   echo "Saving the image ${LAMBDA_NAME_AND_VERSION}"
-  mkdir -p $RELATIVE_ARTIFACT_FOLDER/target
-  docker save "${LAMBDA_NAME_AND_VERSION}" > "${RELATIVE_ARTIFACT_FOLDER}"/target/"${LAMBDA_NAME_AND_VERSION}".tar
+  ARTIFACT_FOLDER="${RELATIVE_ARTIFACT_PATH}"/target
+  echo "Artifact folder: ${ARTIFACT_FOLDER}"
+  mkdir -p ${ARTIFACT_FOLDER}
+  docker save "${LAMBDA_NAME_AND_VERSION}" > "${ARTIFACT_FOLDER}/${LAMBDA_NAME_AND_VERSION}".tar
 }
 
 build() {
