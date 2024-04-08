0f0aa206c2ffa404343d988817fb24a3382b4461
asventetsky <artyom.sven@gmail.com>
2024-04-08 21:19:37 +0200

    Feature#30: update provider version definition

--- misc/build_lambda_zip.sh
+++ misc/build_lambda_zip.sh
@@ -16,27 +16,14 @@ build_zip_archive() {
   cd ..
   zip -r $ARCHIVE_NAME.zip src
 
-  unzip -l $ARCHIVE_NAME.zip
+  ls -l
 
-#  RELATIVE_ARTIFACT_PATH=$1
-#  LAMBDA_NAME_AND_VERSION=$("${RELATIVE_ARTIFACT_PATH}"../../misc/extract_lambda_name_version.sh)
-#
-#  echo "Building the image ${LAMBDA_NAME_AND_VERSION}"
-#  docker build -t "${LAMBDA_NAME_AND_VERSION}" .
-#
-#  echo "Saving the image ${LAMBDA_NAME_AND_VERSION}"
-#  ARTIFACT_FOLDER="${RELATIVE_ARTIFACT_PATH}"target
-#  echo "Artifact folder: $ARTIFACT_FOLDER"
-#  mkdir -p ${ARTIFACT_FOLDER}
-#  docker save "${LAMBDA_NAME_AND_VERSION}" > "${ARTIFACT_FOLDER}/${LAMBDA_NAME_AND_VERSION}".tar
-#
-#  ls -l $ARTIFACT_FOLDER
-#  echo "Current folder: $(pwd)"
+#  unzip -l $ARCHIVE_NAME.zip
 }
 
 build() {
   echo "Running code quality tools"
-  pylint --rcfile="./../.pylintrc" src/ test/
+  pylint --rcfile="./.pylintrc" src/ test/
 
   echo "Running tests"
   python3 -m unittest
