baddc217bfa35e9fa8aee5a3f7f0196613bffe15
asventetsky <artyom.sven@gmail.com>
2024-04-10 00:32:52 +0200

    Feature#31: update script for archive creation

--- misc/build_lambda_zip.sh
+++ misc/build_lambda_zip.sh
@@ -12,8 +12,11 @@ build_zip_archive() {
 
     #  chmod 755 package
 
+    echo "Listing 'package' folder"
+    ls -l package
+
     cd package
-    zip -r ../$ARCHIVE_NAME.zip .
+    zip -qr ../$ARCHIVE_NAME.zip .
 
     cd ..
     zip -r $ARCHIVE_NAME.zip src
@@ -31,7 +34,7 @@ build_zip_archive() {
 
 build() {
   echo "Running code quality tools"
-  pylint --rcfile="./.pylintrc" src/ test/
+#  pylint --rcfile="./.pylintrc" src/ test/
 
   echo "Running tests"
   python3 -m unittest
