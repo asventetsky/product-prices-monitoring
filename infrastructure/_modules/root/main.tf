0a196486d61ca4ab73a6bde0e1dec8931f4b6208
asventetsky <artyom.sven@gmail.com>
2024-04-10 12:39:12 +0200

    Feature#33: fix cognito_auth type

--- infrastructure/_modules/root/main.tf
+++ infrastructure/_modules/root/main.tf
@@ -152,10 +152,7 @@ module "api_gateway" {
   source = "../../../../../_modules/api_gateway"
 
   api_gateway_name = "product-pricess-monitoring-app-${var.region}-${var.env}"
-  cognito_auth = {
-    "enable": true
-    "confirm_email_option": "CONFIRM_WITH_LINK"
-  }
+  cognito_auth = true
   stage = var.env
 
   integrations = {
