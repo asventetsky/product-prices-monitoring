abdefb2b3a7e8cf8623b39108f504f3a3fa4caa6
asventetsky <artyom.sven@gmail.com>
2024-04-13 14:24:01 +0200

    Feature#34: update sign up script

--- misc/cognito_sign_up_user/sign_up_user.py
+++ misc/cognito_sign_up_user/sign_up_user.py
@@ -4,6 +4,14 @@ import os
 client = boto3.client("cognito-idp")
 
 
+def build_user():
+    return {
+        "name": os.environ["COGNITO_USER_NAME"],
+        "password": os.environ["COGNITO_USER_PASSWORD"],
+        "email": os.environ["COGNITO_USER_EMAIL"]
+    }
+
+
 def get_user_pool_id():
     response = client.list_user_pools(MaxResults=1)
     return response['UserPools'][0]['Id']
@@ -17,28 +25,28 @@ def get_user_pool_client_id(user_pool_id):
     return response['UserPoolClients'][0]['ClientId']
 
 
-def sign_up(user_pool_client_id):
-    response = client.sign_up(
+def sign_up(user_pool_client_id, user):
+    client.sign_up(
         ClientId=user_pool_client_id,
-        Username=os.environ["COGNITO_USER_NAME"],
-        Password=os.environ["COGNITO_USER_PASSWORD"],
+        Username=user["name"],
+        Password=user["password"],
         UserAttributes=[
             {
                 'Name': 'email',
-                'Value': os.environ["COGNITO_USER_EMAIL"]
+                'Value': user["email"]
             },
         ],
     )
 
 
 def main():
+    user = build_user()
+    print(f"Signing up `{user['name']}`")
     user_pool_id = get_user_pool_id()
-    print(f"user_pool_id={user_pool_id}")
-
     user_pool_client_id = get_user_pool_client_id(user_pool_id)
-    print(f"user_pool_client_id={user_pool_client_id}")
+    sign_up(user_pool_client_id, user)
 
-    sign_up(user_pool_client_id)
+    print(f"Finished signing up `{user['name']}`")
 
 if __name__ == "__main__":
     main()
