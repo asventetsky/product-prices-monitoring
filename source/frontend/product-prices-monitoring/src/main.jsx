3d8a7a66c05a1677b61c837780d06bee182d8348
asventetsky <artyom.sven@gmail.com>
2024-04-15 01:59:30 +0200

    Feature#35: frontend template update with Dashboard

--- source/frontend/product-prices-monitoring/src/main.jsx
+++ source/frontend/product-prices-monitoring/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import Dashboard from './dashboard/Dashboard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
 )
