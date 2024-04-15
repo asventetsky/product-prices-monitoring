111eb4fb4e5c41222db873278ac52735940c888e
asventetsky <artyom.sven@gmail.com>
2024-04-15 18:10:59 +0200

    Feature#36: update link to parent root resource

--- infrastructure/_modules/api_gateway/main.tf
+++ infrastructure/_modules/api_gateway/main.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_resource" "this_nested" {
   for_each = { for k, v in var.integrations : k => v if v.nested_path }
 
   path_part = split("/", split(" ", each.key)[1])[2]
-  parent_id = aws_api_gateway_resource.this[index(var.integrations, each)].id
+  parent_id = aws_api_gateway_resource.this[index(var.integrations, each.key)].id
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
 
