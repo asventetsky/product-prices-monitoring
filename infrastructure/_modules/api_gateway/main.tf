09f58fc8a4f02312b4a9142f5f98c33cf3cb8053
asventetsky <artyom.sven@gmail.com>
2024-04-15 23:28:33 +0200

    Feature#36: simplify api gateway creation

--- infrastructure/_modules/api_gateway/main.tf
+++ infrastructure/_modules/api_gateway/main.tf
@@ -61,7 +61,7 @@ resource "aws_cognito_user_pool_domain" "main" {
 }
 
 resource "aws_api_gateway_deployment" "this" {
-  depends_on = [aws_api_gateway_integration.root]
+  depends_on = [aws_api_gateway_integration.root, aws_api_gateway_integration.child]
 
   stage_description = md5(file("main.tf"))
   rest_api_id = aws_api_gateway_rest_api.this.id
