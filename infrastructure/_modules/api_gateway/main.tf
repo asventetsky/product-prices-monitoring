97de6071994d7d339134b0fbc2d858edad60caaf
asventetsky <artyom.sven@gmail.com>
2024-04-15 18:52:41 +0200

    Feature#36: fix error

--- infrastructure/_modules/api_gateway/main.tf
+++ infrastructure/_modules/api_gateway/main.tf
@@ -96,7 +96,7 @@ resource "aws_api_gateway_resource" "this_nested" {
   for_each = { for k, v in var.integrations : k => v if v.nested_path }
 
   path_part = split("/", split(" ", each.key)[1])[2]
-  parent_id = aws_api_gateway_resource.this[index(local.integrations_list, each.key)].id
+  parent_id = aws_api_gateway_resource.this[each.key].id
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
 
