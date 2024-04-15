8c05fc5546c864bdfcac0b9ca4b01d70a5ab00c9
asventetsky <artyom.sven@gmail.com>
2024-04-15 18:24:50 +0200

    Feature#36: nested resource

--- infrastructure/_modules/api_gateway/main.tf
+++ infrastructure/_modules/api_gateway/main.tf
@@ -1,3 +1,7 @@
+locals {
+  integrations_list = [for k, r in var.integrations : k]
+}
+
 resource "aws_api_gateway_rest_api" "this" {
   name = var.api_gateway_name
 }
@@ -92,7 +96,7 @@ resource "aws_api_gateway_resource" "this_nested" {
   for_each = { for k, v in var.integrations : k => v if v.nested_path }
 
   path_part = split("/", split(" ", each.key)[1])[2]
-  parent_id = aws_api_gateway_resource.this[index(var.integrations, each.key)].id
+  parent_id = aws_api_gateway_resource.this[index(integrations_list, each.key)].id
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
 
