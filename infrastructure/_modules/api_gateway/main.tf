211c71f860fac67dd003b9434faffb1cba3f3804
asventetsky <artyom.sven@gmail.com>
2024-04-26 21:22:33 +0200

    Feature#37: fix redeployment

--- infrastructure/_modules/api_gateway/main.tf
+++ infrastructure/_modules/api_gateway/main.tf
@@ -68,8 +68,30 @@ resource "aws_cognito_user_pool_domain" "main" {
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [aws_api_gateway_integration.root, aws_api_gateway_integration.child]
 
-  stage_description = md5(file("main.tf"))
   rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.root.id,
+      aws_api_gateway_method.root.id,
+      aws_api_gateway_integration.root.id,
+
+      aws_api_gateway_resource.child.id,
+      aws_api_gateway_method.child.id,
+      aws_api_gateway_integration.child.id,
+
+      aws_api_gateway_resource.cors.id,
+      aws_api_gateway_method.cors.id,
+      aws_api_gateway_integration.cors.id,
+    ]))
+  }
 
   lifecycle {
     create_before_destroy = true
